43df7f7e0e4472def8054eb5720fdf5db5d973d4
Thanabodee <wingyminus@gmail.com>
2011-08-21 20:16:26 +0700

    Do...Loop

---
+++ vb_tut5-Loop/vb_tut5-Loop/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        ' If not keyword Step it's plus 1 to counter
+        Dim finish As Integer = Integer.Parse(Console.ReadLine())
+        For i = 1 To finish
+            For j = 1 To i
+                Console.Write("*")
+            Next j
+            Console.WriteLine()
+        Next i
+
+        ' While Loop
+        Dim counter = 0
+        While counter <= 100
+            Console.WriteLine(counter & " ")
+            counter += 7
+        End While
+
+        counter = 0
+        Do
+            Console.WriteLine(counter)
+            counter += 1
+        Loop Until counter > 10
+
+        counter = 0
+        Do
+            Console.WriteLine(counter)
+            counter += 1
+        Loop While counter <= 10
+    End Sub
+
+End Module
