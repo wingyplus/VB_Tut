ae52198400a6c5c9f360aba9aef3bb59a01b8922
Thanabodee <wingyminus@gmail.com>
2011-08-21 15:29:11 +0700

    If...Then...Else

---
+++ vb_tut4-Control_Flow/vb_tut4-Control_Flow/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        ' If condition [ Then ]
+        '    [ statements ]
+        ' [ ElseIf elseifcondition [ Then ]
+        '    [ elseifstatements ] ]
+        ' [ Else
+        '    [ elsestatements ] ]
+        ' End If
+        ' -or-
+        ' If condition Then [ statements ] [ Else [ elsestatements ] ]
+        Dim test As Integer = Integer.Parse(Console.ReadLine())
+
+        If test >= 80 And test <= 100 Then
+            Console.WriteLine("A")
+        ElseIf test >= 70 And test < 80 Then
+            Console.WriteLine("B")
+        ElseIf test >= 60 And test < 70 Then
+            Console.WriteLine("C")
+        ElseIf test >= 50 And test < 60 Then
+            Console.WriteLine("D")
+        Else
+            Console.WriteLine("F")
+        End If
+    End Sub
+
+End Module
---
+++ vb_tut4-Control_Flow/vb_tut4-Control_Flow/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("vb_tut4-Control_Flow")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("vb_tut4-Control_Flow")> 
+<Assembly: AssemblyCopyright("Copyright ©  2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fc9a1613-9274-43ef-b3d0-c21ddb954482")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
