470b86ef3eb9ac87ad38c194a9bcf060c2c6ab81
Thanabodee <wingyminus@gmail.com>
2011-08-23 00:18:05 +0700

    List and Dictionary

---
+++ vb_tut10-Collections/vb_tut10-Collections/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim list As ICollection(Of String) = New LinkedList(Of String)
+        list.Add("Hello")
+        list.Add("World")
+        list.Add("Grean")
+
+        For Each word In list
+            Console.Write(word & " ")
+        Next
+        Console.WriteLine()
+
+        Dim map As New Dictionary(Of String, String)
+        map.Add("name", "wingyplus")
+        map.Add("username", "grean")
+        Console.WriteLine(map.Item("name"))
+    End Sub
+
+End Module
